562638b70e5d5a3f918a324c3189206ae417622a
kasia <kaska7@tlen.pl>
2017-11-04 20:21:41 +0100

    ex 2, buttons wiecej

--- js/app.js
+++ js/app.js
@@ -1,14 +1,36 @@
 
 'use strict';
 document.addEventListener('DOMContentLoaded', function() {
-    var button = document.querySelector(".for-dropdown");
-    var menu = document.querySelector(".dropdown");
 
-    button.addEventListener("mouseover", function() {
-        menu.style.display = "block";
+    var menu = document.querySelector(".for-dropdown");
+    var list = document.querySelector(".dropdown");
+
+    menu.addEventListener("mouseover", function() {
+        list.style.display = "block";
     });
-    button.addEventListener("mouseout", function() {
-        menu.style.display = "none";
+
+    menu.addEventListener("mouseout", function() {
+        list.style.display = "none";
     })
+
+    var buttons = document.querySelectorAll('.read-more');
+
+    function showHide() {
+
+        var textArea = this.previousElementSibling;
+
+        if (textArea.style.display === 'none' || textArea.style.display === '') {
+            textArea.style.display = 'block';
+            this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+        } else {
+            textArea.style.display = 'none';
+            this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+        }
+    }
+
+    for (var i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener("click", showHide);
+    }
+    //buttons[0].addEventListener("click", showHide);
 
 });
